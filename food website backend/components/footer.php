dfd8e1718d97ad9b5678995816b46cfe07993e0a
Hassan hammam <113901009+HassanHammam01@users.noreply.github.com>
2025-01-30 19:39:15 +0200

    Add files via upload

---
+++ food website backend/components/footer.php
@@ -0,0 +1,42 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:shaikhanas@gmail.com">hassan.hammam134@gmail.com</a>
+         
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>9:00am to 00:10pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="https://www.google.com/maps/place/Aldiwan+restaurant/@33.4720401,34.8190785,9z/data=!4m10!1m2!2m1!1saldiwan+rest!3m6!1s0x151ef37e76008b2f:0x44d2c22904b3db57!8m2!3d33.4681913!4d35.3121262!15sChNhbCBkaXdhbiByZXN0YXVyYW50WhUiE2FsIGRpd2FuIHJlc3RhdXJhbnSSAQpyZXN0YXVyYW504AEA!16s%2Fg%2F11h3zq3s_c?entry=ttu&g_ep=EgoyMDI1MDEyNy4wIKXMDSoASAFQAw%3D%3D" 
+      target="_blank">
+      Sarafand-Aaqbiyeh 
+   </a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:+96171661324">71-661324</a>
+         
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>HASSAN HAMMAM</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
